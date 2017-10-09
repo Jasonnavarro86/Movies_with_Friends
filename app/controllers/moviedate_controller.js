64e2c663a5924a38a41381c2e873e952ecb114bd
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-10-08 23:15:08 -0400

    60% Finished.

--- app/controllers/moviedate_controller.js
+++ app/controllers/moviedate_controller.js
@@ -17,10 +17,38 @@ router.get("/moviedates", function(req,res){
   }
    res.render("index", moviedata);
  })
-})
+});
 
 
+router.post("/newmovie", function(req, res){
 
+    movies.create(
+    [
+        "movie_name", "watched"
+    ],
+    [
+        req.body.name, req.body.watched
+    ],
+    function(result){
+        res.json({ id: result.insertId });
+    });
+
+});
+
+
+router.put("/newmovie/:id", function(req, res){
+    var condition = "id = " + req.params.id;
+
+    movies.update({
+         watched: req.body.watched
+    }, condition, function(result){
+        if(result.changedRows == 0){
+            return res.status(404).end();
+        }else{
+            res.status(200).end();
+        }
+    });
+});
 
 
 module.exports = router;
